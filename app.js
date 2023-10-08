df640343737461bc58d4e8b5f43abcc5a16e9847
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-08 22:54:52 +0800

    menerapkan cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const cors = require("cors");
 
 const router = require("./routes/index.js");
 
@@ -11,6 +12,12 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
 app.use("/", express.static(path.join(__dirname, "FE-Jayapura-28")));
 
 app.listen(port, function() {
