deefafe566582f3f898047e18d5de92dd6249b4e
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-09 07:53:33 +0800

    set access control

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(router);
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 app.use(
     express.urlencoded({
         extended: false,
