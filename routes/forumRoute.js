788b5470459c0f107b015419928c4fff14325fe4
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-08 12:19:28 +0800

    tambah middleware pada route

--- routes/forumRoute.js
+++ routes/forumRoute.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const verifyToken = require("../middleware/verifyToken");
 const forumRouter = express.Router();
 
 const forumController = require("../controller/forumController.js");
 
-forumRouter.get("/forum", forumController.getAllForum);
+forumRouter.get("/forum", verifyToken, forumController.getAllForum);
 
-forumRouter.get("/forum/:id", forumController.getForumById);
+forumRouter.get("/forum/:id", verifyToken, forumController.getForumById);
 
-forumRouter.post("/forum", forumController.addForum);
+forumRouter.post("/forum", verifyToken, forumController.addForum);
 
-forumRouter.patch("/forum/:id", forumController.updateForum);
+forumRouter.patch("/forum/:id", verifyToken, forumController.updateForum);
 
-forumRouter.delete("/forum/:id", forumController.deleteForum);
+forumRouter.delete("/forum/:id", verifyToken, forumController.deleteForum);
 
 module.exports = forumRouter;
