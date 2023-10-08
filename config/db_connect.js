eb8e28a71c158db7c6559b2a353568bbc70ff844
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-09 00:23:21 +0800

    pindah ca.pem ke config

--- config/db_connect.js
+++ config/db_connect.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const db = new Sequelize(
     "mysql://avnadmin:AVNS_MG8QMzemeSX6T6Qydo2@mysql-34e0c795-capstone-28.aivencloud.com:11734/defaultdb?ssl-mode=REQUIRED", {
-        ssl: fs.readFileSync(path.join(__dirname, "../ca.pem")),
+        ssl: fs.readFileSync(path.join(__dirname, "ca.pem")),
         dialect: "mysql",
         logging: true,
     }
