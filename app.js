ee710bf955305c60793c8e5526ea7a65229e7bb3
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-09 06:38:51 +0800

    hapus cors

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const path = require("path");
-const cors = require("cors");
 
 const router = require("./routes/index.js");
 const db = require("./config/db_connect.js");
@@ -11,22 +9,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins = [
-    "https://kampus-merdeka-software-engineering.github.io/FE-Jayapura-28/",
-];
-const corsOptions = {
-    origin: function(origin, callback) {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-};
-
 app.use(express.json());
 app.use(router);
-app.use(cors(corsOptions));
+
 app.use(
     express.urlencoded({
         extended: false,
