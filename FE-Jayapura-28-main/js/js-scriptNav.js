fab80d58ed6f1a3a858554e73f13709e5dcccdb7
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-09 14:59:43 +0800

    hps fe

--- FE-Jayapura-28-main/js/js-scriptNav.js
+++
@@ -1,9 +0,0 @@
-// Ambil elemen tombol hamburger dan daftar menu
-const menuToggle = document.getElementById("menuToggle");
-const navList = document.getElementById("menu");
-
-// Tambahkan event listener untuk tombol hamburger
-menuToggle.addEventListener("click", function() {
-    // Toggle class "active" pada daftar menu
-    navList.classList.toggle("active");
-});
