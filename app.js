320fa6dfe619dfa38cd00259befddc73a025eddc
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-09 11:00:55 +0800

    corsss

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const cors = require("cors");
 
 const router = require("./routes/index.js");
 const db = require("./config/db_connect.js");
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
