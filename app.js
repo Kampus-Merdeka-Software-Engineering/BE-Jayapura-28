2a077a920522b9939df8539811c2701f96a6b344
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-09 11:18:19 +0800

    corsss

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Private-Network", true);
     next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const cors = require("cors");
 
 router.use(cors());
 
+router.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    next();
+});
+
 const saranRouter = require("./saranRoute.js");
 const artikelRouter = require("./artikelRoute.js");
 const userRouter = require("./userRoute.js");
