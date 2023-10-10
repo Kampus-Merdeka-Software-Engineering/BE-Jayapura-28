5aee8a53ebb2aac8f956e826f84d7ec43ddd85c7
anugrahwd <nurisnainianugrah01@gmail.com>
2023-10-10 13:01:35 +0800

    tambah cors

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,6 +1,18 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
+const app = express.Router();
+const cors = require("cors");
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    next();
+});
 
 function getTokenFromHeader(req) {
     const authHeader = req.headers.authorization;
